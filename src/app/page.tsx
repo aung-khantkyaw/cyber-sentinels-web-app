b7c3cc67b49a134f100d0cfa1bd0d55aeac4cdfb
Tun Tun Myint <tunt72553@gmail.com>
2025-06-30 23:48:44 +0630

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return <div className="font-bold">
+    Home Page
+    <Button>Click me</Button>
+  </div>;
+};
+
+export default Page;
